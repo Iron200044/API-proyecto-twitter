3ebb985c29e99dcc9a6780a34c5eda5f063931aa
Iron200044 <samuelacga@unisabana.edu.co>
2024-05-21 22:01:24 -0500

    CORS all origins

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 // Configurar CORS
 app.use(cors({
-    origin: ['https://newtwitter-xi.vercel.app', 'http://localhost:5173'],
+    origin: '*',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
     allowedHeaders: 'Content-Type,Authorization' // Encabezados permitidos
