25cc4dc9aade8b68175776af3250cfe8fe69e1f0
Iron200044 <samuelacga@unisabana.edu.co>
2024-05-23 03:12:59 -0500

    cors final

--- index.js
+++ index.js
@@ -14,9 +14,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Configurar CORS
+const allowedOrigins = [
+  "http://localhost:5173/",
+  "https://fp-daw-frontend.vercel.app/"
+];
   app.use(
     cors({
-      origin: '*',
+      origin: allowedOrigins,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Include OPTIONS method
       credentials: true, // If you're using cookies or sessions
     })
